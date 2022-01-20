21fe834328cb076ccb9bf2b7df78b4b02502f6cb
matts-github <mattwoodhouse@live.co.uk>
2022-01-20 12:35:48 +0000

    html, CSS and JS update

--- js/main.js
+++ js/main.js
@@ -17,24 +17,78 @@ getBtn.addEventListener('click', getData);
 
 // Form Validation
 
+// function validation()
+// {
+
+//     var email = document.getElementById("email").value;
+//     var pattern = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
+
+//     if (email.match(pattern))
+//     {
+//         form.classList.add("valid")
+//         form.classList.remove("invalid")
+//         text.innerHTML = "Your Email Address in Valid.";
+//         text.style.color = "#00ff00";
+//     }
+//     else
+//     {
+//         form.classList.remove("valid")
+//         form.classList.add("invalid")
+//         text.innerHTML = "Please Enter Valid Email Address.";
+//         text.style.color = "#ff0000";
+//     }
+// }
+
 function validation()
 {
+  var form = document.getElementById("form");
+  var email = document.getElementById("email").value;
+  var text = document.getElementById("text");
+  var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
 
-    var email = document.getElementById("email").value;
-    var pattern = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
+  if (email.match(pattern))
+  {
+    form.classList.add("valid");
+    form.classList.remove("invalid");
+    text.innerHTML = "Your Email Address is Valid.";
+    text.style.color = "#00ff00";
+  }
+  else
+  {
+    form.classList.remove("valid");
+    form.classList.add("invalid");
+    text.innerHTML = "Please Enter Valid Email Address";
+    text.style.color = "#ff0000";
+  }
 
-    if (email.match(pattern))
-    {
-        form.classList.add("valid")
-        form.classList.remove("invalid")
-        text.innerHTML = "Your Email Address in Valid.";
-        text.style.color = "#00ff00";
-    }
-    else
-    {
-        form.classList.remove("valid")
-        form.classList.add("invalid")
-        text.innerHTML = "Please Enter Valid Email Address.";
-        text.style.color = "#ff0000";
-    }
+  if (email == "")
+  {
+    form.classList.remove("valid");
+    form.classList.remove("invalid");
+    text.innerHTML = "";
+    text.style.color = "#00ff00";
+  }
 }
+
+let movies = [];
+
+const addMovie = (ev)=>{
+  ev.preventDefault(); // To stop the form from submitting
+  let movie = {
+      id: Date.now(),
+      email: document.getElementById('email').value
+  }
+  movies.push(movie);
+  document.forms[0].reset(); // To clear the form for the next entries
+
+  // For display purposes only
+  console.warn('added' , {movies} );
+  let pre = document.querySelector('#msg pre');
+  pre.textContent = '\n' + JSON.stringify(movies, '\t', 2);
+
+  // Saving to local storage
+  localStorage.setItem('MyMovieList', JSON.stringify(movies) );
+}
+document.addEventListener('DOMContentLoaded', ()=>{
+  document.getElementById('post-btn').addEventListener('click', addMovie);
+});
